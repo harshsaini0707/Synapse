8b8416235edc95e0a7c54b5c16035534ee8cdc8b
harshsaini0707 <harshsaini0172@gmail.com>
2025-09-17 09:19:21 +0530

    tanstack setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,16 @@
 'use client'
 import { SessionProvider } from "next-auth/react"
 import "./globals.css"
+import QueryProvider from "@/providers/QueryProvider"
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html>
       <body>
         <SessionProvider>
-          {children}
+          <QueryProvider>
+      {children}
+          </QueryProvider>
+        
         </SessionProvider>
       </body>
     </html>

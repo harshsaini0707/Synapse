b69042135b3baa1fbbad894e28b2b97d8139e64d
harshsaini0707 <harshsaini0172@gmail.com>
2025-10-30 18:54:39 +0530

    test mode dodo

--- src/lib/dodopayment.ts
+++ src/lib/dodopayment.ts
@@ -1,9 +1,5 @@
 import DodoPayments from "dodopayments";
 export const dodopayments = new DodoPayments({
-  bearerToken:
-    process.env.NODE_ENV === "development"
-      ? process.env.DODO_API_KEY_TEST
-      : process.env.DODO_API_KEY_TEST, 
-  environment:
-    process.env.NODE_ENV === "development" ? "test_mode" : "test_mode", 
+  bearerToken: process.env.DODO_API_KEY_TEST,
+  environment: "test_mode",
 });
